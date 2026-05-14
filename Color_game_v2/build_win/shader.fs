44a2b7b51e33e743678b468b468eecddf228f1f7
Sagit Bolat <sagitbolat@gmail.com>
2026-05-14 02:22:47 +0200

    added some of the legacy levels back to the game

---
+++ Color_game_v2/build_win/shader.fs
@@ -0,0 +1,10 @@
+#version 330 core
+in vec2 tex_coord;
+in vec4 color_multiplier;
+out vec4 FragColor;
+uniform sampler2D texture1;
+void main()
+{
+    FragColor = texture(texture1, tex_coord) * color_multiplier;
+    if (FragColor.a < 0.01) discard;
+}
